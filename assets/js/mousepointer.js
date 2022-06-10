39f3c45d1aaaaa3ccfa7f6aea2b842866cfc687b
Hanbit Kang <hanbitkang95@gmail.com>
2022-06-10 11:33:05 -0300

    HTML + CSS + JS: Mousepointer

---
+++ assets/js/mousepointer.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const cursor = document.querySelector('.mousepointer');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+} );
