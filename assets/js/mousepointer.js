d4841a81b669366e5ab29113241509bdbfd7cdda
Hanbit Kang <hanbitkang95@gmail.com>
2022-06-21 11:42:08 -0300

    HTML + CSS: Clean codes

--- assets/js/mousepointer.js
+++ assets/js/mousepointer.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const cursor = document.querySelector('.mousepointer');
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
-} );
+// document.addEventListener('mousemove', (e) => {
+//     cursor.style.left = e.clientX + 'px';
+//     cursor.style.top = e.clientY + 'px';
+// } );
+
+document.addEventListener('mousemove', function(e){
+    cursor.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+});
