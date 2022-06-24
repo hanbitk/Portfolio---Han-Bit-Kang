fc31c457235267da4100adda86d9907f116af5fd
Hanbit Kang <hanbitkang95@gmail.com>
2022-06-24 14:43:05 -0300

    HTML + CSS + JS: Logo changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,35 @@ history.scrollRestoration = 'manual';
         };
     };
 
+/***********************************************************/
+
 // Fix Navbar
 var scrollpos = window.scrollY;
 var nav = document.querySelector('nav');
-
 
 window.addEventListener('scroll', function(){
     const nav = document.querySelector('nav');
     nav.classList.toggle('sticky', window.scrollY > 0);
 });
 
+/***********************************************************/
+
+// Change image on click - logo off and on
+window.addEventListener("DOMContentLoaded", function () {
+    const powerBtn = document.querySelector('.power__btn');
+    powerBtn.addEventListener('click', changeLogo);
+});
+
+function changeLogo(){
+    const logoOff = document.getElementById('logo__off');
+    if(logoOff.src.match("/assets/images/logo/thin_light_logo.png")){
+        logoOff.src = "/assets/images/logo/thin_green_logo.png";
+    }else{
+        logoOff.src = "/assets/images/logo/thin_light_logo.png";
+    }
+}
+
+/***********************************************************/
 
 // ScrollReveal
 ScrollReveal().reveal('#about', {duration: 2000});
